91bdd2da76b08ad92435c7ae9e7ceae0b952636a
Katrine <katrine.oes@gmail.com>
2025-10-31 12:31:17 +0100

    projekt der gemmer målinger i en fil

---
+++ DataSkema/DataSkema/DataLogger.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataSkema
+{
+    public static class DataLogger
+    {
+        // Her bruger vi bare den aktuelle mappe, i stedet for AppDataDirectory
+        private static string filePath = Path.Combine(Environment.CurrentDirectory, "Vandladningsskema.csv");
+
+        public static void AppendMeasurement(Measurement m)
+        {
+            string line = m.ToString();
+            File.AppendAllText(filePath, line + Environment.NewLine); //AppendAllTekst tilføjer en ny linje til filen
+        }
+
+        public static string GetFilePath()
+        {
+            return filePath;
+        }
+    }
+}
+
---
+++ DataSkema/DataSkema/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace DataSkema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // ALT der står her inde er kun for at teste at det virker
+            // Her burde main hellere køre en klasse som har modtaget data fra arduino, når dette kan lade sig gøre
+            Console.WriteLine(" Test af datalogger ");
+
+            // Testmålinger
+            Measurement m1 = new Measurement("Ble", 156.4);
+            Measurement m2 = new Measurement("Urin", 80.6);
+            Measurement m3 = new Measurement("Væske", 130.9);
+
+            // Gemmer dem
+            DataLogger.AppendMeasurement(m1);
+            DataLogger.AppendMeasurement(m2);
+            DataLogger.AppendMeasurement(m3);
+
+            Console.WriteLine("Målinger gemt!");
+            Console.WriteLine($"Fil gemt her: {DataLogger.GetFilePath()}");
+
+            // Viser fil indholdet
+            Console.WriteLine("\nIndhold i filen:");
+            Console.WriteLine(File.ReadAllText(DataLogger.GetFilePath()));
+        }
+    }
+}
+
+
